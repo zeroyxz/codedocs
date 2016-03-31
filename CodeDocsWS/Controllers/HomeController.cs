ca8dcf601192c6969dd076085cf98bb2ac23571c
zeroyxz <Peterworlin@hotmail.com>
2016-03-31 13:29:31 +0100

    Added instrumentation and App Settings

--- CodeDocsWS/Controllers/HomeController.cs
+++ CodeDocsWS/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Diagnostics;
 using System.Web.Mvc;
 
 namespace CodeDocsWS.Controllers
@@ -10,11 +11,13 @@ namespace CodeDocsWS.Controllers
     {
         public ActionResult Index()
         {
+            Trace.TraceInformation("In Index function that returns Actionresult");
             return View();
         }
 
         public ActionResult About()
         {
+            Trace.TraceInformation("In About funciton that returns ActionResult");
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +25,7 @@ namespace CodeDocsWS.Controllers
 
         public ActionResult Contact()
         {
+            Trace.TraceInformation("In contact function that returns Actionresult");
             ViewBag.Message = "Your contact page.";
 
             return View();
